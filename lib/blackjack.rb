30a7533d93414d69af197adaecdc31d2ecb09944
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-10 15:30:54 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -53,5 +53,11 @@ end
 #####################################################
 
 def runner
-  # code runner here
+  welcome
+  card_total = initial_round
+  until card_total > 21
+  card_total = hit?(card_total)
+  display_card_total(card_total)
+    end
+  end_game(card_total)
 end
