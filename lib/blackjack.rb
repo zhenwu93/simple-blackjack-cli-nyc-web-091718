66bb703540a5d41c02f66225a9b46e7201d0c352
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-10 14:33:42 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -36,8 +36,8 @@ def hit?(total)
   prompt_user
   input = get_user_input
   if input == "h"
-    new_card = deal_card
-    total = total + new_card
+    deal_card
+    total = total + deal_card
     return total
   end
 end
