1f569d360742dfe4f6ceb1aec63ad5e1d74506bf
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-10 15:03:38 +0000

    automatically backed up by learn

--- lib/runner.rb
+++ lib/runner.rb
@@ -2,16 +2,12 @@ require_relative "blackjack.rb"
 
 def runner
   welcome
-  initial_round
-
-  card_total = 0
+  card_total = initial_round
   until card_total > 21
-  hit?
-  card_total += deal_card
-  puts display_card_total
-
+  card_total = hit?(card_total)
+  display_card_total(card_total)
+    end
   end_game
-  end
 end
 
 runner
