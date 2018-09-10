2f0dde54a096e04824449f4b2feefb65c4642407
Zhen Wei Wu <zhenwu93@gmail.com>
2018-09-10 15:24:36 +0000

    automatically backed up by learn

--- lib/runner.rb
+++ lib/runner.rb
@@ -4,7 +4,7 @@ def runner
   welcome
   card_total = initial_round
   until card_total > 21
-  card_total = hit?
+  card_total = hit?(card_total)
   display_card_total(card_total)
     end
   end_game(card_total)
